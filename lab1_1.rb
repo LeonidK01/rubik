222b258010a6089a49f154048b72e9645687c53b
Leonid <en.lenya@mail.ru>
2022-02-06 17:58:43 +0300

    Задание 1.2
    Использован аргумент для приветствия и рассмотрен класс String

--- lab1_1.rb
+++ lab1_1.rb
@@ -2,5 +2,12 @@
 print - Выводит строчку без \n
 =end
 # puts выводит и делает переход на следующию строку
-puts "Рello world_puts"
-print "hello world_print"
+#puts "Рello world_puts"
+#print "hello world_print"
+
+puts "Доброго времени суток #{ARGV[0]}"
+
+puts String.instance_methods
+
+puts "--------------------------------"
+puts String.class.instance_methods
