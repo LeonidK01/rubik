83e24ac8611e96962c2bbd64873ecf68984cdecd
Leonid <en.lenya@mail.ru>
2022-02-07 00:08:59 +0300

    Задание 1.3
    Разобраны условные операторы и конструкция case

--- lab1_1.rb
+++ lab1_1.rb
@@ -5,9 +5,37 @@ print - Выводит строчку без \n
 #puts "Рello world_puts"
 #print "hello world_print"
 
-puts "Доброго времени суток #{ARGV[0]}"
+# puts "Доброго времени суток #{ARGV[0]}"
 
-puts String.instance_methods
+# puts String.instance_methods
 
-puts "--------------------------------"
-puts String.class.instance_methods
+# puts "--------------------------------"
+# puts String.class.instance_methods
+
+puts "Введите ваш любимы язык программирования"
+
+lang= gets.chomp
+
+if lang=="Ruby"
+	puts "Вы подлиза"
+elsif lang=="Python"
+	puts "Вы хороши собой"
+elsif lang=="С++"
+	puts "Вы выбрали путь ситха"
+elsif lang=="С#"
+	puts "Это хороший выбор"
+elsif lang=="Prolog"
+	puts "А ты хорош"
+elsif lang=="Kotlin"
+	puts "Любишь Андроид ? :("
+end
+
+puts "Скоро полюбишь руби" if lang != "Ruby"
+
+
+case lang
+when "Ruby"
+	puts "Вы подлиза"
+when "Python"
+	puts "Вы хороши собой"
+end
