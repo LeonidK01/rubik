95d4837a5b13a62413ebb047f14175eaf79c7755
Leonid <en.lenya@mail.ru>
2022-02-23 11:57:36 +0300

    Задание 2.3
    Добавлен 3 номер

--- lab1_2.rb
+++ lab1_2.rb
@@ -40,6 +40,78 @@ def method_max(x)
 	return max
 end
 
-puts method_sum(x)
-puts method_min(x)
-puts method_max(x)
+def neprost(x)
+	for i in (2..x/2)
+		if x%i==0
+			return true
+		end
+	end
+	return false
+end
+
+def method_neprost_del(x)
+	sum=0
+	for i in (2..x/2)
+		if x%i==0 and neprost(i)
+			sum=sum+i
+		end
+	end
+	if neprost(x)
+		sum=sum+x
+	end
+	return sum
+end
+
+def method_min3(x)
+	min3=0
+	while x>0
+		if x%10<3
+			min3=min3+1
+		end
+		x=x/10
+	end
+	return min3
+end
+
+def nod(a,b)
+	while a!=0 and b!=0
+		if a>b
+			a=a%b
+		else
+			b=b%a
+		end
+	end
+	return a+b
+end
+
+def method_sum_prost(x)
+	sum=0
+	while x>0 and neprost(x)==false
+		sum=sum+(x%10)
+		x=x/10
+	end
+	return sum
+end
+
+def method_last(x)
+	sum_pros=method_sum_prost(x)
+	count=0
+	for i in (1..x)
+		if x%i!=0 and nod(x,i)!=1 and nod(sum_pros,i)==1
+			count = count + 1
+		end
+	end
+	return count
+end
+
+
+
+if ARGV[1]==nil
+	puts "Hello word!"
+else
+	puts eval(ARGV[1]+"("+x.to_s+")")
+end
+# puts method_sum(x)
+# puts method_min(x)
+# puts method_max(x)
+puts method_last(x)
