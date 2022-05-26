9e012e67c65d721c3a2bf9a373e01f73a4f65dd0
Leonid <en.lenya@mail.ru>
2022-05-26 18:12:05 +0300

    Проект (версия 2)
    Добавлено управления бд и выбор форм

--- Project/Form1.Designer.cs
+++ Project/Form1.Designer.cs
@@ -28,13 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button_done = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox_pic = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button_done
+            // 
+            this.button_done.Location = new System.Drawing.Point(337, 233);
+            this.button_done.Name = "button_done";
+            this.button_done.Size = new System.Drawing.Size(75, 23);
+            this.button_done.TabIndex = 5;
+            this.button_done.Text = "Выбор";
+            this.button_done.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(334, 147);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Выбор таблицы";
+            // 
+            // comboBox_pic
+            // 
+            this.comboBox_pic.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_pic.FormattingEnabled = true;
+            this.comboBox_pic.Items.AddRange(new object[] {
+            "1. Клиент",
+            "2. Машины",
+            "3. Аренда"});
+            this.comboBox_pic.Location = new System.Drawing.Point(320, 184);
+            this.comboBox_pic.Name = "comboBox_pic";
+            this.comboBox_pic.Size = new System.Drawing.Size(121, 21);
+            this.comboBox_pic.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_done);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox_pic);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button_done;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox_pic;
     }
 }
 
--- Project/Form1.cs
+++ Project/Form1.cs
@@ -7,14 +7,93 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Project
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
+
+        public partial class Form1 : Form
         {
-            InitializeComponent();
+            Fasad fasad;
+            MySqlConnection con;
+            MySqlCommand command;
+            public Form1()
+            {
+                InitializeComponent();
+
+                this.fasad = new Fasad();
+                this.con = new MySqlConnection("server=127.0.0.1;username=root;password=root;database=arenda");
+                podkl();
+
+            }
+
+            void podkl()
+            {
+                string zapros = "SELECT * FROM arenda_";
+                DataTable table = new DataTable();
+                MySqlDataAdapter adp = new MySqlDataAdapter(zapros, con);
+                adp.Fill(table);
+                foreach (DataRow row in table.Rows)
+                {
+                    fasad.create_arenda_list(row.ItemArray);
+                }
+                zapros = "SELECT * FROM car";
+                adp = new MySqlDataAdapter(zapros, con);
+                table = new DataTable();
+                adp.Fill(table);
+                foreach (DataRow row in table.Rows)
+                {
+                    fasad.create_car_list(row.ItemArray);
+                }
+                zapros = "SELECT * FROM client";
+                adp = new MySqlDataAdapter(zapros, con);
+                table = new DataTable();
+                adp.Fill(table);
+                foreach (DataRow row in table.Rows)
+                {
+                    fasad.create_client_list(row.ItemArray);
+                }
+            }
+
+            public Form1(Fasad fasad)
+            {
+                InitializeComponent();
+
+                this.fasad = fasad;
+                this.con = new MySqlConnection("server=127.0.0.1;username=root;password=root;database=arenda");
+
+            }
+
+            private void Label1_Click(object sender, EventArgs e)
+            {
+
+            }
+
+            private void ComboBox_pic_SelectedIndexChanged(object sender, EventArgs e)
+            {
+
+            }
+
+            private void Button_done_Click(object sender, EventArgs e)
+            {
+                if (comboBox_pic.SelectedIndex == 0)
+                {
+                    Client per_1 = new Client(fasad);
+                    per_1.Show();
+                    this.Visible = false;
+                }
+                if (comboBox_pic.SelectedIndex == 1)
+                {
+                    Car per_1 = new Car(fasad);
+                    per_1.Show();
+                    this.Visible = false;
+                }
+                if (comboBox_pic.SelectedIndex == 2)
+                {
+                    Arenda per_1 = new Arenda(fasad);
+                    per_1.Show();
+                    this.Visible = false;
+                }
+            }
         }
-    }
 }
