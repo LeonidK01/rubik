84201e9dc4502bc049188045a92d3a131b8f422f
Leonid <en.lenya@mail.ru>
2022-05-26 21:10:20 +0300

    Проект(4)
    Добавлена форма для машин и контролер

---
+++ Project/Car.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Project
+{
+    partial class Car
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_up = new System.Windows.Forms.Button();
+            this.btn_del = new System.Windows.Forms.Button();
+            this.button_ins = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView_car = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_car)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_up
+            // 
+            this.btn_up.Location = new System.Drawing.Point(594, 356);
+            this.btn_up.Name = "btn_up";
+            this.btn_up.Size = new System.Drawing.Size(75, 23);
+            this.btn_up.TabIndex = 45;
+            this.btn_up.Text = "Обновить";
+            this.btn_up.UseVisualStyleBackColor = true;
+            // 
+            // btn_del
+            // 
+            this.btn_del.Location = new System.Drawing.Point(675, 299);
+            this.btn_del.Name = "btn_del";
+            this.btn_del.Size = new System.Drawing.Size(75, 23);
+            this.btn_del.TabIndex = 44;
+            this.btn_del.Text = "Удалить";
+            this.btn_del.UseVisualStyleBackColor = true;
+            // 
+            // button_ins
+            // 
+            this.button_ins.Location = new System.Drawing.Point(511, 299);
+            this.button_ins.Name = "button_ins";
+            this.button_ins.Size = new System.Drawing.Size(75, 23);
+            this.button_ins.TabIndex = 43;
+            this.button_ins.Text = "Добавить";
+            this.button_ins.UseVisualStyleBackColor = true;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(663, 177);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 42;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(696, 151);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 41;
+            this.label5.Text = "Class";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(663, 103);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 40;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(696, 78);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 13);
+            this.label6.TabIndex = 39;
+            this.label6.Text = "Milleage";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(499, 242);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 38;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(532, 216);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 37;
+            this.label3.Text = "brend";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(499, 177);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 36;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(532, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 35;
+            this.label2.Text = "marka";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(499, 103);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 34;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(532, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 33;
+            this.label1.Text = "id_car";
+            // 
+            // dataGridView_car
+            // 
+            this.dataGridView_car.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_car.Location = new System.Drawing.Point(38, 72);
+            this.dataGridView_car.Name = "dataGridView_car";
+            this.dataGridView_car.Size = new System.Drawing.Size(381, 231);
+            this.dataGridView_car.TabIndex = 32;
+            // 
+            // Car
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_up);
+            this.Controls.Add(this.btn_del);
+            this.Controls.Add(this.button_ins);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView_car);
+            this.Name = "Car";
+            this.Text = "Car";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_car)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_up;
+        private System.Windows.Forms.Button btn_del;
+        private System.Windows.Forms.Button button_ins;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView_car;
+    }
+}
---
+++ Project/Car.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Project
+{
+    public partial class Car : Form
+    {
+        Fasad fasad;
+        MySqlConnection con;
+        MySqlCommand command;
+        public Car(Fasad fas)
+        {
+            InitializeComponent();
+
+            this.fasad = fas;
+            this.con = new MySqlConnection("server=127.0.0.1;username=root;password=root;database=arenda");
+            podkl();
+        }
+
+        void init_vue()
+        {
+            this.dataGridView_car.Columns.Add("id_car", "id_client");
+            this.dataGridView_car.Columns.Add("brend", "brend");
+            this.dataGridView_car.Columns.Add("marka", "marka");
+            this.dataGridView_car.Columns.Add("mileage", "mileage");
+            this.dataGridView_car.Columns.Add("class", "class");
+        }
+        void podkl()
+        {
+            init_vue();
+            data_resause();
+        }
+
+        void data_resause()
+        {
+            this.dataGridView_car.Rows.Clear();
+            List<List<string>> mass_car = this.fasad.car_List.mass_car();
+            foreach (List<string> i in mass_car)
+            {
+                dataGridView_car.Rows.Add(i[0], i[1], i[2], i[3], i[4]);
+            }
+
+        }
+
+        private void DataGridView_client_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+
+        private void Button_bec_Click(object sender, EventArgs e)
+        {
+            Form1 dr = new Form1(this.fasad);
+            dr.Show();
+            Hide();
+        }
+
+        private void Button_ins_Click(object sender, EventArgs e)
+        {
+            int id_car = Convert.ToInt32(fasad.client_List.ret_id());
+            string brend = textBox2.Text;
+            string marka = textBox3.Text;
+            int brobeg = Convert.ToInt32(textBox6.Text);
+            string class_ = textBox5.Text;
+            if (el_is_valid(brend, marka, class_) && (!fasad.car_List.get_full_id().Contains(id_car.ToString())))
+            {
+                Object[] mass = { id_car, brend, marka, brobeg, class_ };
+                fasad.create_car_list(mass);
+                string insert_query = string.Format("INSERT INTO car(id_car,brend,marka,milleage,class) VALUES (" + id_car + ",\"" + brend + "\",\"" + marka + "\",\"" + brobeg + "\",\"" + class_ + "\")");
+                executeMyQuery(insert_query);
+
+            }
+            data_resause();
+        }
+
+
+        private bool el_is_valid(string brend, string marka, string class_)
+        {
+            bool c = true;
+            if (brend == "" || marka == "" || class_ == "")
+            {
+                c = false;
+            }
+            return c;
+        }
+        private void Btn_del_Click(object sender, EventArgs e)
+        {
+            fasad.car_List.delete_el(Convert.ToInt32(textBox1.Text));
+            string insert_query = string.Format("DELETE FROM `car` WHERE `id_car`={0}", Convert.ToInt32(textBox1.Text));
+            executeMyQuery(insert_query);
+            data_resause();
+        }
+
+        private void Car_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_up_Click(object sender, EventArgs e)
+        {
+            int id_car = Convert.ToInt32(textBox1.Text);
+            string brend = textBox2.Text;
+            string marka = textBox3.Text;
+            int brobeg = Convert.ToInt32(textBox6.Text);
+            string class_ = textBox5.Text;
+            if (el_is_valid(brend, marka, class_) && (fasad.car_List.get_full_id().Contains(id_car.ToString())))
+            {
+                Object[] mass = { id_car, brend, marka, brobeg, class_ };
+                fasad.update_car_list(mass);
+                string insert_query = string.Format("UPDATE car SET `id_car`={0},`brend`='{1}',`marka`='{2}',`milleage`='{3}',`class`='{4}' WHERE `id_client`={0}", id_car.ToString(), brend, marka, brobeg, class_);
+                executeMyQuery(insert_query);
+
+            }
+            data_resause();
+        }
+
+        public void executeMyQuery(string query)
+        {
+            try
+            {
+                openConnection();
+                command = new MySqlCommand(query, con);
+                if (command.ExecuteNonQuery() == 1)
+                {
+                    MessageBox.Show("Выполннено");
+
+                }
+                else
+                {
+                    MessageBox.Show("Не выполненно");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                closeConnection();
+            }
+        }
+        public void openConnection()
+        {
+            if (con.State == ConnectionState.Closed)
+                con.Open();
+        }
+        public void closeConnection()
+        {
+            if (con.State == ConnectionState.Open)
+                con.Close();
+        }
+    }
+}
