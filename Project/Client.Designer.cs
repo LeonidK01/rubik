f40106f65ecb1570fd27b71262a29391fc531ec7
Leonid <en.lenya@mail.ru>
2022-05-26 19:49:14 +0300

    Проект(3)
    Добавлена форма для клиента и контролер

---
+++ Project/Client.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace Project
+{
+    partial class Client
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_up = new System.Windows.Forms.Button();
+            this.btn_del = new System.Windows.Forms.Button();
+            this.button_ins = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView_client = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_client)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_up
+            // 
+            this.btn_up.Location = new System.Drawing.Point(584, 363);
+            this.btn_up.Name = "btn_up";
+            this.btn_up.Size = new System.Drawing.Size(75, 23);
+            this.btn_up.TabIndex = 32;
+            this.btn_up.Text = "Обновить";
+            this.btn_up.UseVisualStyleBackColor = true;
+            // 
+            // btn_del
+            // 
+            this.btn_del.Location = new System.Drawing.Point(665, 306);
+            this.btn_del.Name = "btn_del";
+            this.btn_del.Size = new System.Drawing.Size(75, 23);
+            this.btn_del.TabIndex = 31;
+            this.btn_del.Text = "Удалить";
+            this.btn_del.UseVisualStyleBackColor = true;
+            // 
+            // button_ins
+            // 
+            this.button_ins.Location = new System.Drawing.Point(501, 306);
+            this.button_ins.Name = "button_ins";
+            this.button_ins.Size = new System.Drawing.Size(75, 23);
+            this.button_ins.TabIndex = 30;
+            this.button_ins.Text = "Добавить";
+            this.button_ins.UseVisualStyleBackColor = true;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(653, 249);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 29;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(677, 223);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 28;
+            this.label4.Text = "Телефон";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(653, 184);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 27;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(686, 158);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 13);
+            this.label5.TabIndex = 26;
+            this.label5.Text = "Дата получения";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(653, 110);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 25;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(677, 85);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 13);
+            this.label6.TabIndex = 24;
+            this.label6.Text = "Фамилия";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(489, 249);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 23;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(522, 223);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Отчество";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(489, 184);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 21;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(522, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Имя";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(489, 110);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 19;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(522, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Id";
+            // 
+            // dataGridView_client
+            // 
+            this.dataGridView_client.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_client.Location = new System.Drawing.Point(26, 65);
+            this.dataGridView_client.Name = "dataGridView_client";
+            this.dataGridView_client.Size = new System.Drawing.Size(399, 265);
+            this.dataGridView_client.TabIndex = 17;
+            // 
+            // Client
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_up);
+            this.Controls.Add(this.btn_del);
+            this.Controls.Add(this.button_ins);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView_client);
+            this.Name = "Client";
+            this.Text = "Client";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_client)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_up;
+        private System.Windows.Forms.Button btn_del;
+        private System.Windows.Forms.Button button_ins;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView_client;
+    }
+}
---
+++ Project/Client.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Text.RegularExpressions;
+
+namespace Project
+{
+    public partial class Client : Form
+    {
+        Fasad fasad;
+        MySqlConnection con;
+        MySqlCommand command;
+        public Client(Fasad fas)
+        {
+            InitializeComponent();
+
+            this.fasad = fas;
+            this.con = new MySqlConnection("server=127.0.0.1;username=root;password=root;database=arenda");
+            podkl();
+
+        }
+
+        void podkl()
+        {
+            init_vue();
+            data_resause();
+        }
+
+        void init_vue()
+        {
+            this.dataGridView_client.Columns.Add("id_client", "id_client");
+            this.dataGridView_client.Columns.Add("name", "name");
+            this.dataGridView_client.Columns.Add("first_name", "first_name");
+            this.dataGridView_client.Columns.Add("last_name", "last_name");
+            this.dataGridView_client.Columns.Add("data_accept", "data_accept");
+            this.dataGridView_client.Columns.Add("phone", "phone");
+        }
+        void data_resause()
+        {
+            this.dataGridView_client.Rows.Clear();
+            List<List<string>> mass_client = this.fasad.client_List.mass_client();
+            foreach (List<string> i in mass_client)
+            {
+                dataGridView_client.Rows.Add(i[0], i[1], i[2], i[3], i[4], i[5]);
+            }
+
+        }
+
+        private void DataGridView_client_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Button_back_Click(object sender, EventArgs e)
+        {
+            Form1 dr = new Form1(this.fasad);
+            dr.Show();
+            Hide();
+        }
+
+        private void Button_ins_Click(object sender, EventArgs e)
+        {
+            int id_client = Convert.ToInt32(fasad.client_List.ret_id());
+            string name = textBox2.Text;
+            string first_name = textBox3.Text;
+            string last_name = textBox6.Text;
+            string stag = textBox5.Text;
+            string phone = textBox4.Text;
+            if (el_is_valid(name, first_name, last_name, phone) && (!fasad.client_List.get_full_id().Contains(id_client.ToString())))
+            {
+                Object[] mass = { id_client, name, first_name, last_name, stag, phone };
+                fasad.create_client_list(mass);
+                string insert_query = string.Format("INSERT INTO client(id_client,name,first_name,last_name,data_accept,phone) VALUES (" + id_client + ",\"" + name + "\",\"" + first_name + "\",\"" + last_name + "\",\"" + stag + "\",\"" + phone + "\")");
+                executeMyQuery(insert_query);
+
+            }
+            data_resause();
+
+        }
+
+        private bool el_is_valid(string name, string first_name, string last_name, string number)
+        {
+            bool c = true;
+            string regex = @"[0-9]{11}";
+            if (!Regex.IsMatch(number, regex))
+            {
+                c = false;
+            }
+            if (name == "" || first_name == "" || last_name == "")
+            {
+                c = false;
+            }
+            return c;
+
+        }
+
+        private void Btn_del_Click(object sender, EventArgs e)
+        {
+            fasad.client_List.delete_el(Convert.ToInt32(textBox1.Text));
+            string insert_query = string.Format("DELETE FROM `client` WHERE `id_client`={0}", Convert.ToInt32(textBox1.Text));
+            executeMyQuery(insert_query);
+            data_resause();
+        }
+
+        private void Btn_up_Click(object sender, EventArgs e)
+        {
+            int id_client = Convert.ToInt32(textBox1.Text);
+            string name = textBox2.Text;
+            string first_name = textBox3.Text;
+            string last_name = textBox6.Text;
+            int stag = Convert.ToInt32(textBox5.Text);
+            string phone = textBox4.Text;
+            if (el_is_valid(name, first_name, last_name, phone) && (fasad.client_List.get_full_id().Contains(id_client.ToString())))
+            {
+                Object[] mass = { id_client, name, first_name, last_name, stag, phone };
+                fasad.update_client_list(mass);
+                string insert_query = string.Format("UPDATE client SET `id_client`={0},`name`='{1}',`first_name`='{2}',`last_name`='{3}',`data_accept`='{4}',`phone`='{5}' WHERE `id_client`={0}", id_client.ToString(), name, first_name, last_name, stag, phone);
+                executeMyQuery(insert_query);
+
+            }
+            data_resause();
+        }
+
+        public void executeMyQuery(string query)
+        {
+            try
+            {
+                openConnection();
+                command = new MySqlCommand(query, con);
+                if (command.ExecuteNonQuery() == 1)
+                {
+                    MessageBox.Show("Выполннено");
+
+                }
+                else
+                {
+                    MessageBox.Show("Не выполненно");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                closeConnection();
+            }
+        }
+        public void openConnection()
+        {
+            if (con.State == ConnectionState.Closed)
+                con.Open();
+        }
+        public void closeConnection()
+        {
+            if (con.State == ConnectionState.Open)
+                con.Close();
+        }
+    }
+}
